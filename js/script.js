38e71e87704de2bbeec876d9ffa450de15e101e8
Amrilazim3 <a.azim0711@gmail.com>
2022-11-24 14:08:47 +0800

    insert partials component into main html file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
- 
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
-    nav.classList.toggle('flex')
-    nav.classList.toggle('hidden')
-})
+const navbar = document.querySelector('.navbar')
+
+async function fetchComponent() {
+    await fetch('./partials/navbar.html')
+        .then(res => res.text())
+        .then(data => {
+            navbar.innerHTML = data;
+        })
+
+    const footer = document.querySelector('.footer')
+
+    await fetch('./partials/footer.html')
+        .then(res => res.text())
+        .then(data => {
+            footer.innerHTML = data;
+        })
+
+    const menuBtn = document.getElementById('menu-btn')
+    const menu = document.getElementById('menu')
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('open')
+        menu.classList.toggle('flex')
+        menu.classList.toggle('hidden')
+    })
+}
+
+fetchComponent();
